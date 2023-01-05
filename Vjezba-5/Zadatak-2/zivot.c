9576f709c585c69b345160d03bbec5fca5a8cb0e
TihomirPop <tihomirpopovic42@gmail.com>
2023-01-05 23:58:29 +0100

    5. vjezba 2. zadatak sve osim jedenja

--- Vjezba-5/Zadatak-2/zivot.c
+++ Vjezba-5/Zadatak-2/zivot.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
 
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -11,6 +12,13 @@ void* tin();
 void* davor();
 void* ivica();
 void* ivan();
+
+void spavanje(const char* ime);
+void programiranje(const char* ime);
+void servis(const char* ime);
+void televizija(const char* ime);
+void tenis(const char* ime);
+void klavir(const char* ime);
 
 int main(int argc, char *argv[]){
     pthread_t opisnik[4];
@@ -32,4 +40,78 @@ int main(int argc, char *argv[]){
             printf("Greska u pthread_join()!\n");
 
     return 0;
+}
+
+void* tin(){
+    const char* ime = "tin";
+    srand((unsigned)time(NULL));
+}
+
+void* davor(){
+    const char* ime = "davor";
+    srand((unsigned)time(NULL));
+}
+
+void* ivica(){
+    const char* ime = "ivica";
+    srand((unsigned)time(NULL));
+}
+
+void* ivan(){
+    const char* ime = "ivan";
+    srand((unsigned)time(NULL));
+}
+
+
+void spavanje(const char* ime){
+    printf("%s sada spava\n", ime);
+    fflush(stdout);
+
+    if(strcmp(ime, "tin"))
+        sleep(rand() % 6 + 5);
+    else
+        sleep(rand() % 4 + 7);
+}
+
+void programiranje(const char* ime){
+    printf("%s sada programira\n", ime);
+    fflush(stdout);
+
+    int faktorijel = 1;
+    for(int i = rand() % 10 + 1; i >= 1; i--)
+        faktorijel *= i;
+}
+
+void servis(const char* ime){
+    printf("%s sada vozi auto na servis\n", ime);
+    fflush(stdout);
+
+    int kvar = rand() % 100;
+    if(kvar < 25)
+        sleep(4);
+    else if(kvar < 45)
+        sleep(2);
+    else
+        sleep(rand() % 3 + 2);
+}
+
+void televizija(const char* ime){
+    printf("%s sada gleda tv\n", ime);
+    fflush(stdout);
+
+    sleep(rand() % 3 + 2);
+}
+
+void tenis(const char* ime){
+    printf("%s sada igra tenis\n", ime);
+    fflush(stdout);
+
+    sleep(rand() % 3 + 2);
+}
+
+void klavir(const char* ime){
+    printf("%s sada slusa sviranje klavira\n", ime);
+    fflush(stdout);
+
+    sleep(rand() % 3 + 2);
 }
